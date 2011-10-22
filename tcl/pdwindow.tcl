3792671582f510b72883307ef21ae547a741b75b
Hans-Christoph Steiner <hans@eds.org>
2011-10-22 00:30:54 -0400

    ported pd-gui-rewrite Pd window to Pd-extended
    - cleaned up header layout to make Sofy happy ;)

--- tcl/pdwindow.tcl
+++ tcl/pdwindow.tcl
@@ -17,6 +17,7 @@ namespace eval ::pdwindow:: {
     namespace export create_window
     namespace export pdtk_post
     namespace export pdtk_pd_dsp
+    namespace export pdtk_pd_meters
     namespace export pdtk_pd_dio
 }
 
@@ -190,8 +191,10 @@ proc ::pdwindow::pdtk_pd_dsp {value} {
     # TODO canvas_startdsp/stopdsp should really send 1 or 0, not "ON" or "OFF"
     if {$value eq "ON"} {
         set ::dsp 1
+        .pdwindow.header.frame.dsp configure -background green
     } else {
         set ::dsp 0
+        .pdwindow.header.frame.dsp configure -background lightgray
     }
 }
 
@@ -202,6 +205,57 @@ proc ::pdwindow::pdtk_pd_dio {red} {
         .pdwindow.header.dio configure -foreground lightgray
     }
         
+}
+
+#--VU meter procedures---------------------------------------------------------#
+
+proc ::pdwindow::calc_vu_color {position} {
+    if {$position > 32} {
+        return "#DF3A32"
+    } elseif {$position > 30} {
+        return "#EEAD54"
+    } elseif {$position > 25} {
+        return "#E9E448"
+    } elseif {$position > 0} {
+        return "#65DF37"
+    } else {
+        return "#6CEFF1"
+    }
+}
+
+proc ::pdwindow::create_vu {tkcanvas name} {
+    for {set i 0} {$i<40} {incr i} {
+        set x [expr $i * 4]
+        $tkcanvas create line $x 2 $x 18 -width 3 -tag "$name$i" \
+            -fill [calc_vu_color $i] -state hidden
+    }
+}
+
+proc ::pdwindow::set_vu {name db} {
+    # TODO figure out the actual math here to properly map the incoming values
+    set elements [expr ($db + 1) / 3]
+    for {set i 0} {$i<$elements} {incr i} {
+        .pdwindow.header.vu.$name itemconfigure "$name$i" -state normal
+    }
+    for {set i $elements} {$i<40} {incr i} {
+        .pdwindow.header.vu.$name itemconfigure "$name$i" -state hidden
+    }
+}
+
+proc ::pdwindow::pdtk_pd_meters {indb outdb inclip outclip} {
+#    puts stderr [concat meters $indb $outdb $inclip $outclip]
+    set_vu in $indb
+    if {$inclip == 1} {
+        .pdwindow.header.cliplabel.in configure -background red
+    } else {
+        .pdwindow.header.cliplabel.in configure -background lightgray
+    }
+    set_vu out $outdb
+    if {$outclip == 1} {
+        .pdwindow.header.cliplabel.out configure -background red
+    } else {
+        .pdwindow.header.cliplabel.out configure -background lightgray
+    }
 }
 
 #--bindings specific to the Pd window------------------------------------------#
@@ -216,6 +270,14 @@ proc ::pdwindow::pdwindow_bindings {} {
     # Select All doesn't seem to work unless its applied to the whole window
     bind .pdwindow <$::modifier-Key-a> ".pdwindow.text tag add sel 1.0 end"
     # the "; break" part stops executing another binds, like from the Text class
+    bind .pdwindow.text <Key-Tab> "focus .pdwindow.tcl.entry; break"
+
+    # bindings for the Tcl entry widget
+    bind .pdwindow.tcl.entry <$::modifier-Key-a> "%W selection range 0 end; break"
+    bind .pdwindow.tcl.entry <Return> "::pdwindow::eval_tclentry"
+    bind .pdwindow.tcl.entry <Up>     "::pdwindow::get_history 1"
+    bind .pdwindow.tcl.entry <Down>   "::pdwindow::get_history -1"
+    bind .pdwindow.tcl.entry <KeyRelease> +"::pdwindow::validate_tcl"
 
     # these don't do anything in the Pd window, so alert the user, then break
     # so no more bindings run
@@ -322,17 +384,17 @@ proc ::pdwindow::create_window {} {
     set ::loaded(.pdwindow) 0
 
     # colorize by class before creating anything
-    option add *PdWindow*Entry.highlightBackground "grey" startupFile
-    option add *PdWindow*Frame.background "grey" startupFile
-    option add *PdWindow*Label.background "grey" startupFile
-    option add *PdWindow*Checkbutton.background "grey" startupFile
-    option add *PdWindow*Menubutton.background "grey" startupFile
+    option add *PdWindow*Entry.highlightBackground "lightgray" startupFile
+    option add *PdWindow*Frame.background "lightgray" startupFile
+    option add *PdWindow*Label.background "lightgray" startupFile
+    option add *PdWindow*Checkbutton.background "lightgray" startupFile
+    option add *PdWindow*Menubutton.background "lightgray" startupFile
     option add *PdWindow*Text.background "white" startupFile
     option add *PdWindow*Entry.background "white" startupFile
 
     toplevel .pdwindow -class PdWindow
-    wm title .pdwindow [_ "Pd"]
-    set ::windowname(.pdwindow) [_ "Pd"]
+    wm title .pdwindow [_ "Pd-extended"]
+    set ::windowname(.pdwindow) [_ "Pd-extended"]
     if {$::windowingsystem eq "x11"} {
         wm minsize .pdwindow 400 75
     } else {
@@ -341,26 +403,44 @@ proc ::pdwindow::create_window {} {
     wm geometry .pdwindow =500x400+20+50
     .pdwindow configure -menu .menubar
 
-    frame .pdwindow.header -borderwidth 1 -relief flat -background lightgray
+    frame .pdwindow.header -borderwidth 1 -relief flat
     pack .pdwindow.header -side top -fill x -ipady 5
-
-    frame .pdwindow.header.pad1
-    pack .pdwindow.header.pad1 -side left -padx 12
-
-    checkbutton .pdwindow.header.dsp -text [_ "DSP"] -variable ::dsp \
-        -font {$::font_family 18 bold} -takefocus 1 -background lightgray \
+    frame .pdwindow.header.spacer -borderwidth 0
+    pack .pdwindow.header.spacer -side left -fill y -anchor w -ipadx 1
+    checkbutton .pdwindow.header.meters -variable ::meters -takefocus 1 \
+        -command {pdsend "pd meters $::meters"}
+    pack .pdwindow.header.meters -side left -fill y -anchor w -padx 5 -pady 10
+    frame .pdwindow.header.vu -borderwidth 0
+    pack .pdwindow.header.vu -side left
+    canvas .pdwindow.header.vu.in -width 150 -height 20 -background "#3F3F3F" \
+        -highlightthickness 1 -highlightbackground lightgray
+    create_vu .pdwindow.header.vu.in "in"
+    canvas .pdwindow.header.vu.out -width 150 -height 20 -background "#3F3F3F" \
+        -highlightthickness 1 -highlightbackground lightgray
+    create_vu .pdwindow.header.vu.out "out"
+    pack .pdwindow.header.vu.in -side top
+    pack .pdwindow.header.vu.out -side top
+    frame .pdwindow.header.cliplabel -borderwidth 0
+    pack .pdwindow.header.cliplabel -side left
+    label .pdwindow.header.cliplabel.in -text [_ "IN"] -width 4
+    label .pdwindow.header.cliplabel.out -text [_ "OUT"] -width 4
+    pack .pdwindow.header.cliplabel.in .pdwindow.header.cliplabel.out -side top
+    frame .pdwindow.header.frame -borderwidth 0 -padx 5
+    pack .pdwindow.header.frame -side right
+    checkbutton .pdwindow.header.frame.dsp -text [_ "DSP"] -variable ::dsp \
+        -font {$::font_family 18 bold} -takefocus 1 -padx 5 \
         -borderwidth 0  -command {pdsend "pd dsp $::dsp"}
-    pack .pdwindow.header.dsp -side right -fill y -anchor e -padx 5 -pady 0
+    pack .pdwindow.header.frame.dsp -side right -fill y -anchor e -padx 5 -pady 0
 # DIO button
     label .pdwindow.header.dio -text [_ "audio I/O error"] -borderwidth 0 \
-        -background lightgray -foreground lightgray \
-        -takefocus 0 \
-        -font {$::font_family 14}
+        -foreground lightgray -takefocus 0 -font {$::font_family 14}
     pack .pdwindow.header.dio -side right -fill y -padx 30 -pady 0
 
-    label .pdwindow.header.loglabel -text [_ "Log:"] -anchor e \
-        -background lightgray
-    pack .pdwindow.header.loglabel -side left
+# Tcl entry box and log level frame
+    frame .pdwindow.tcl -borderwidth 0
+    pack .pdwindow.tcl -side bottom -fill x
+    frame .pdwindow.tcl.pad
+    pack .pdwindow.tcl.pad -side right -padx 12
 
     set loglevels {0 1 2 3 4}
     lappend logmenuitems "0 [_ fatal]"
@@ -369,18 +449,24 @@ proc ::pdwindow::create_window {} {
     lappend logmenuitems "3 [_ debug]"
     lappend logmenuitems "4 [_ all]"
     set logmenu \
-        [eval tk_optionMenu .pdwindow.header.logmenu ::loglevel $loglevels]
-    .pdwindow.header.logmenu configure -background lightgray
+        [eval tk_optionMenu .pdwindow.tcl.logmenu ::loglevel $loglevels]
+    .pdwindow.tcl.logmenu configure
     foreach i $loglevels {
         $logmenu entryconfigure $i -label [lindex $logmenuitems $i]
     }
     trace add variable ::loglevel write ::pdwindow::filter_buffer_to_text
 
     # TODO figure out how to make the menu traversable with the keyboard
-    #.pdwindow.header.logmenu configure -takefocus 1
-    pack .pdwindow.header.logmenu -side left
-    frame .pdwindow.tcl -borderwidth 0
-    pack .pdwindow.tcl -side bottom -fill x
+    #.pdwindow.tcl.logmenu configure -takefocus 1
+    pack .pdwindow.tcl.logmenu -side right
+    label .pdwindow.tcl.loglabel -text [_ "Log:"] -anchor e
+    pack .pdwindow.tcl.loglabel -side right
+    label .pdwindow.tcl.label -text [_ "Tcl:"] -anchor e
+    pack .pdwindow.tcl.label -side left
+    entry .pdwindow.tcl.entry -width 200 \
+        -exportselection 1 -insertwidth 2 -insertbackground blue \
+        -textvariable ::pdwindow::tclentry -font {$::font_family 12}
+    pack .pdwindow.tcl.entry -side left -fill x
 # TODO this should use the pd_font_$size created in pd-gui.tcl    
     text .pdwindow.text -relief raised -bd 2 -font {-size 10} \
         -highlightthickness 0 -borderwidth 1 -relief flat \
