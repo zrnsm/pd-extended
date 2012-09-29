pure-data/pure-data

fix compiler ouch in s_loader.c

src/s_loader.c
@@ -169,33 +169,35 @@ gotone:
     filename[MAXPDSTRING-1] = 0;
 
 #ifdef _WIN32
-    sys_bashfilename(filename, filename);
-    /* set the dirname as DllDirectory, meaning in the path for
-       loading other DLLs so that dependent libraries can be included
-       in the same folder as the external. SetDllDirectory() needs a
-       minimum supported version of Windows XP SP1 for
-       SetDllDirectory, so WINVER must be 0x0502 */
-    char dirname[MAXPDSTRING];
-    strncpy(dirname, filename, MAXPDSTRING);
-    char* s = strrchr(dirname, '\\');
-    char* basename = s;
-    if (s && *s)
-      *s = '\0';
-    if (!SetDllDirectory(dirname))
-       error("Could not set '%s' as DllDirectory(), '%s' might not load.",
-             dirname, basename);
-    /* now load the DLL for the external */
-    ntdll = LoadLibrary(filename);
-    if (!ntdll)
     {
-        post("%s: couldn't load", filename);
-        class_set_extern_dir(&s_);
-        return (0);
+        char dirname[MAXPDSTRING], *s, *basename;
+        sys_bashfilename(filename, filename);
+        /* set the dirname as DllDirectory, meaning in the path for
+           loading other DLLs so that dependent libraries can be included
+           in the same folder as the external. SetDllDirectory() needs a
+           minimum supported version of Windows XP SP1 for
+           SetDllDirectory, so WINVER must be 0x0502 */
+        strncpy(dirname, filename, MAXPDSTRING);
+        s = strrchr(dirname, '\\');
+        basename = s;
+        if (s && *s)
+          *s = '\0';
+        if (!SetDllDirectory(dirname))
+           error("Could not set '%s' as DllDirectory(), '%s' might not load.",
+                 dirname, basename);
+        /* now load the DLL for the external */
+        ntdll = LoadLibrary(filename);
+        if (!ntdll)
+        {
+            post("%s: couldn't load", filename);
+            class_set_extern_dir(&s_);
+            return (0);
+        }
+        makeout = (t_xxx)GetProcAddress(ntdll, symname);  
+        if (!makeout)
+             makeout = (t_xxx)GetProcAddress(ntdll, "setup");
+        SetDllDirectory(NULL); /* reset DLL dir to nothing */
     }
-    makeout = (t_xxx)GetProcAddress(ntdll, symname);  
-    if (!makeout)
-         makeout = (t_xxx)GetProcAddress(ntdll, "setup");
-    SetDllDirectory(NULL); /* reset DLL dir to nothing */
 #elif defined HAVE_LIBDL
     dlobj = dlopen(filename, RTLD_NOW | RTLD_GLOBAL);
     if (!dlobj)

zrnsm/pd-extended

src/s_loader.c
@@ -173,33 +173,35 @@ gotone:
     filename[MAXPDSTRING-1] = 0;
 
 #ifdef _WIN32
-    sys_bashfilename(filename, filename);
-    /* set the dirname as DllDirectory, meaning in the path for
-       loading other DLLs so that dependent libraries can be included
-       in the same folder as the external. SetDllDirectory() needs a
-       minimum supported version of Windows XP SP1 for
-       SetDllDirectory, so WINVER must be 0x0502 */
-    char dirname[MAXPDSTRING];
-    strncpy(dirname, filename, MAXPDSTRING);
-    char* s = strrchr(dirname, '\\');
-    char* basename = s;
-    if (s && *s)
-      *s = '\0';
-    if (!SetDllDirectory(dirname))
-	error("Could not set '%s' as DllDirectory(), '%s' might not load.",
-	      dirname, basename);
-    /* now load the DLL for the external */
-    ntdll = LoadLibrary(filename);
-    if (!ntdll)
     {
-        error("%s: couldn't load", filename);
-        class_set_extern_dir(&s_);
-        return (0);
+        char dirname[MAXPDSTRING], *s, *basename;
+        sys_bashfilename(filename, filename);
+        /* set the dirname as DllDirectory, meaning in the path for
+           loading other DLLs so that dependent libraries can be included
+           in the same folder as the external. SetDllDirectory() needs a
+           minimum supported version of Windows XP SP1 for
+           SetDllDirectory, so WINVER must be 0x0502 */
+        strncpy(dirname, filename, MAXPDSTRING);
+        s = strrchr(dirname, '\\');
+        basename = s;
+        if (s && *s)
+          *s = '\0';
+        if (!SetDllDirectory(dirname))
+           error("Could not set '%s' as DllDirectory(), '%s' might not load.",
+                 dirname, basename);
+        /* now load the DLL for the external */
+        ntdll = LoadLibrary(filename);
+        if (!ntdll)
+        {
+            error("%s: couldn't load", filename);
+            class_set_extern_dir(&s_);
+            return (0);
+        }
+        makeout = (t_xxx)GetProcAddress(ntdll, symname);  
+        if (!makeout)
+             makeout = (t_xxx)GetProcAddress(ntdll, "setup");
+        SetDllDirectory(NULL); /* reset DLL dir to nothing */
     }
-    makeout = (t_xxx)GetProcAddress(ntdll, symname);  
-    if(!makeout)
-        makeout = (t_xxx)GetProcAddress(ntdll, "setup");
-    SetDllDirectory(NULL); /* reset DLL dir to nothing */
 #elif defined HAVE_LIBDL
     dlobj = dlopen(filename, RTLD_NOW | RTLD_GLOBAL);
     if (!dlobj)